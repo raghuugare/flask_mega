1330198981eaa789d87af2fe2bfbe25129ae2b1d
Raghu Ugare <raghu.ugare@gmail.com>
2017-05-29 14:20:40 +0530

    Part 2. Templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello pa!"
+    user = {'nickname' : 'Rags' }
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Post 1'
+        },
+        {
+            'author': {'nickname': 'Amanda'},
+            'body': 'Post 2'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+  
